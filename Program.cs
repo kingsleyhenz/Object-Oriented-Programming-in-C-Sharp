a48b1b7522ff5b5172ebe8ee285d67a3390f14c8
BLACKHENZ29 <ayehenz29@gmail.com>
2022-09-30 10:22:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Object_Oriented_Programming
+{
+    public class Mainly
+    {
+       public static void Main(String[] args)
+        {
+            Student std1 = new Student();
+            std1.firstname = "Obinna";
+            std1.lastname = "Kings";
+            std1.age = 20;
+
+            Student std2 = new Student();
+            std2.firstname = "Buhari";
+            std2.lastname = "Mumu";
+            std2.age = 90;
+
+            Console.WriteLine("Your Name is " + std1.firstname + " " + std1.lastname + " " + std1.age);
+            Console.ReadLine();
+        } 
+    }
+
+}
---
+++ Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Object_Oriented_Programming
+{
+    public class Student
+    {
+        public String firstname;
+        public String lastname;
+        public int age;
+    }
+
+}
