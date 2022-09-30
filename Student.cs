cf6fef76a5dc0d6803d1ae374afdaf67322c538d
BLACKHENZ29 <ayehenz29@gmail.com>
2022-09-30 10:32:22 +0100

    Learning About Constructors

--- Student.cs
+++ Student.cs
@@ -1,10 +1,23 @@
-﻿namespace Object_Oriented_Programming
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming
 {
     public class Student
     {
-        public String firstname;
-        public String lastname;
+        public string firstname;
+        public string lastname;
         public int age;
+     public Student(string firstname, string lastname, int age)
+    {
+        firstname = firstname;
+        lastname = lastname;
+        age = age;
     }
 
+
+    }
 }
